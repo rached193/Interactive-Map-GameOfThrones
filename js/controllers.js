2d70852d9340019f04cd2e91ccddfecfd7c97281
rached193 <archer193@hotmail.com>
2015-06-30 16:26:02 +0200

    Leyenda del mapa, interaccion con el mapa

--- js/controllers.js
+++ js/controllers.js
@@ -4,24 +4,6 @@ var app = angular.module('app-web', [
 
 
 
-
-
-app.factory('queryPortada', function($rootScope, $http, $q) {
-  var deferred = $q.defer();
-  $http.get('rest/query')
-  .success(function(data, status, headers, config) {
-    $rootScope.series = data;
-    /*
-    $rootScope.series = [{Title: 'Full Metal Alchemist', Poster: "full-metal.jpg", Type:"TV", Plot: "Fullmetal Alchemist (鋼の錬金術師 Hagane no Renkinjutsushi?, lit. «el alquimista de acero», también conocida como Full Metal Alchemist, FMA o Hagaren, abreviatura en japonés)"},
-                      {Title: 'Arslan Senki: The Heroic Legend of Arslan', Poster: "arslan.jpg", Type:"TV", Plot:"Esta es la historia de los viajes del Príncipe Arislan, el heredero al imperio de Palse. Al principio, Palse y los Lusitanos están en guerra. Mediante la traición, el Reino de Palse cae derrotado por los Lusitanos. "},
-                      {Title: 'One piece', Poster:"onepiece.jpg",Type:"TV", Plot:"La serie habla sobre un chico llamado Monkey D. Luffy, en su niñez obtuvo poderes elásticos al comer una Akuma no mi (Fruta del Diablo ). Inspirado por su amigo el pirata Shanks comienza un viaje para alcanzar su sueño: ser el Rey de los Piratas. Ahora recorre los mares con el sueño de atravesar el Grand Line, un mar peligroso y desconocido donde Gol D. Roger (difunto rey de los piratas) escondió el One Piece, un tesoro de proporciones inimaginables y que ha sido por décadas la aspiración de los más poderosos y peligrosos piratas entre quienes hasta el momento ninguno ha logrado obtener"}
-                      ];
-                      */
-    deferred.resolve();
-  });
-  return deferred.promise;
-});
-
 //Constantes
 var addr = "192.168.1.2:8090";
 var partial = "view/"
@@ -68,6 +50,22 @@ app.controller("ControladorPortada",['$scope','$cookies',function($scope,$cookie
 
 
 app.controller("ControladorMapa",['$scope',function($scope){
+  var provinces = ["ah","arb","bg","bi","bit","bla","blu","boc","boi","bos","bwk","bzb","cas","cpp","cid","cks",
+  "cw","dm","dra","drm","dus","ess","eyr","ff","fgs","fin","fmi","frozen_short","gde","gol","gul","gwk","gww",
+  "gww-ec","gww-wc","har","hh","hig","hrl","ib","kar","kl","lan","mc","mc-ec","mc-wc","mns","mom","nns","north_of_wall",
+  "old","ork","pyk","rea","rfk","rr","rws","san","sbb","sea","sha","shivering_sea","si","sil","ska","sns","sod","sp","ss",
+  "sta","sto","sun","tar","tor","tri","ts","twi","voa","vr","wal","wal-ec","wal-wc","wh","win","win-ec","win-wc","wss","yro"
+];
+
+$scope.createDummyData = function () {
+    var dataTemp = {};
+    angular.forEach(provinces, function (province, key) {
+        dataTemp[province] = {value: Math.random()}
+    });
+    $scope.dummyData = dataTemp;
+};
+$scope.createDummyData();
+}]);
 }]);
 
 app.controller("ControladorSignUp", ['$scope','$http', '$location', function($scope, $http, $location){
--- js/directives.js
+++ js/directives.js
@@ -1,9 +1,29 @@
 angular.module('app-web').directive('svgMap', ['$compile', function ($compile) {
-    return {
-        restrict: 'A',
-        templateUrl: '/westeros.svg',
-        link: function (scope, element, attrs) {
-
-        }
-    }
+  return {
+      restrict: 'A',
+      templateUrl: 'westeros.svg',
+      link: function (scope, element, attrs) {
+          var regions = element[0].querySelectorAll('.state');
+          angular.forEach(regions, function (path, key) {
+              var regionElement = angular.element(path);
+              regionElement.attr("region", "");
+              $compile(regionElement)(scope);
+          })
+      }
+  }
+}]);
+angular.module('app-web').directive('region', ['$compile', function ($compile) {
+  return {
+      restrict: 'A',
+      scope: true,
+      link: function (scope, element, attrs) {
+          scope.elementId = element.attr("id");
+          scope.regionClick = function () {
+              alert(scope.elementId);
+          };
+          element.attr("ng-click", "regionClick()");
+          element.removeAttr("region"); //Region
+          $compile(element)(scope);
+      }
+  }
 }]);
