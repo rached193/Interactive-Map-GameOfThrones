bfe28c2caf0c02ffcb97b5fc3196a15853317838
rached193 <archer193@hotmail.com>
2016-11-03 12:37:53 +0100

    Mover controlador Login

--- webapp/js/controllers.js
+++ webapp/js/controllers.js
@@ -1,34 +1,3 @@
-
-
-
-app.controller("ControladorLogin", ['$scope', '$http', '$location', '$cookies', '$rootScope', function ($scope, $http, $location, $cookies, $rootScope) {
-    $rootScope.tab = 4;
-
-    $scope.submitLogin = function () {
-        var user = {
-            name: $scope.nombre,
-            password: $scope.pass,
-        };
-        $http.post('/api/v1/login', user)
-            .success(function (data, status, headers, config) {
-                $cookies.user = data.nickname;
-                $cookies.casa = data.casa;
-                $cookies.personaje = data.personaje;
-                if (data.sexo == "hombre") {
-                    $cookies.sexo = "Lord";
-                } else if (data.sexo == "mujer") {
-                    $cookies.sexo = "Lady";
-                }
-                $location.path("/index");
-            })
-            .error(function () {
-                alert("Usuario o contraseña Incorrectas");
-            })
-    };
-
-}]);
-
-
 app.controller("ControladorSelecionarCasa", ['$scope', '$cookies', '$http', '$location', '$rootScope','initData', function ($scope, $cookies, $http, $location, $rootScope,initData) {
     $rootScope.tab = 4;
 
